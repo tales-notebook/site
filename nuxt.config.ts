a305dd1c11e09685434e790baa6c166903612d1a
Henrique de Oliveira <henriqueoliwork@gmail.com>
2023-09-25 14:48:59 -0300

    feat: use GitHub base repo on nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: [
-    '../base-nuxt'
+    process.env.EXTEND_BASE_NUXT_URL || 'github:tales-notebook/base-nuxt'
   ],
   modules: ['@nuxt/content', 'nuxt-gtag'],
   gtag: {
