7aca9fd2936dd8535728a03ece83f1bbf6cc8552
Henrique de Oliveira <henriqueoliwork@gmail.com>
2023-09-25 11:23:36 -0300

    feat: about section

---
+++ components/HomeAbout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center" >
+        <div class="max-w-[500px] px-5">
+            <div class="text-4xl lg:text-6xl font-bold mb-5 lg:mb-10">Our mission</div>
+            
+            <div class="w-full text-lg lg:text-lg flex flex-wrap space-y-5">
+                <p>
+                    Our goal is to make accessing stories from around the world as easy and comfortable as possible.
+                </p>
+                <p>
+                    We wanna provide a platform for creators to share their stories with the world.
+                </p>
+
+                <p>
+                    For Translators to help the stories to reach more people.
+                </p>
+
+                <p>
+                    And for readers to support the creators and translators they like and have a good time reading and sharing their thoughts about the stories.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
